f508841b4153e7304ac650b9e5356c1553c3417b
1LolaOla <funmiolay1@gmail.com>
2023-05-21 08:01:36 -0700

    city.py

--- models/city.py
+++ models/city.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""
-Module containing the City class
-"""
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage_type
+from uuid import uuid4
 
 
 class City(BaseModel, Base):
-    """
-    City class inherits from BaseModel and Base
-    """
+    ''' defines City class '''
     __tablename__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    places = relationship("Place", cascade="all, delete", backref="cities", passive_deletes=True)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+
+        def __init__(self, **kwargs):
+            self.id = str(uuid4())
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+    else:
+        name = ''
+        state_id = ''
