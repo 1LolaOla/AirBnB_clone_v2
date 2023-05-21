12d14086b17f1e9500c153d1cdfff12e4a7241cf
1LolaOla <funmiolay1@gmail.com>
2023-05-21 10:08:46 -0700

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application that displays states and cities.
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the database session after each request."""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays a HTML page with a list of all states."""
+    states = storage.all('State').values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_cities(id):
+    """Displays a HTML page with cities of a specific state."""
+    state = storage.get('State', id)
+    if state is None:
+        return render_template('9-states.html', state=None)
+    cities = state.cities
+    return render_template('9-states.html', state=state, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
